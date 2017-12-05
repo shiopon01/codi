dd4392b5a56a86dda2b043d7366bd22b3a93395e
shiopon01 <shio.0323@gmail.com>
2017-12-05 13:43:05 +0900

    tree

--- main.go
+++ main.go
@@ -5,22 +5,89 @@ import (
   "flag"
 )
 
+// tree
+type Node struct {
+  token bool
+  text string
+  left, right *Node
+}
 
+type Tree struct {
+  root *Node
+}
+
+func newTree () *Tree {
+  return new(Tree)
+}
+
+func newNode (text string) *Node {
+  n := new(Node)
+  n.token = containsToken(text)
+  n.text = text
+  return n
+}
+
+func (t *Tree) insertTree (text string) {
+  t.root = insertNode(t.root, text)
+}
+
+func insertNode (node *Node, text string) *Node {
+  switch {
+  case node == nil:
+    return newNode(text)
+  case node.left == nil:
+    node.left = insertNode(node.left, text)
+  default:
+    node.right = insertNode(node.right, text)
+  }
+  return node
+}
+
+
+func (t *Tree) printTree () {
+  root := t.root
+  if root != nil {
+    fmt.Println("0", root.token, root.text)
+    if root.left != nil {
+      printNode(root.left, 1)
+    }
+    if root.right != nil {
+      printNode(root.right, 1)
+    }
+  }
+}
+
+func printNode (n *Node, count int) {
+  fmt.Println(count, n.token, n.text)
+
+  count += 1
+  if n.left != nil {
+    printNode(n.left, count)
+  }
+  if n.right != nil {
+    printNode(n.right, count)
+  }
+}
+
+// main
 func main () {
   flag.Parse()
   text := flag.Args()[0]
 
   split := parse(text)
-  box := createBox(split)
+  split.printTree()
 
+  box := createBox(split)
   for _, v := range box {
     fmt.Println(v)
   }
 }
 
 // split
-func parse (text string) []string {
-  var result []string
+func parse (text string) *Tree {
+  // var result []string
+  tree := newTree()
+
   str, tbuf, tfound := "", "", false // tbuf => token buffer
 
   for _, c := range text {
@@ -59,8 +126,8 @@ func parse (text string) []string {
 
     if len(tbuf) > 2 {
       if containsToken(tbuf) {
-        result = append(result, str)
-        result = append(result, tbuf)
+        tree.insertTree(tbuf)
+        tree.insertTree(str)
         str, tbuf = "", ""
       } else {
         str += tbuf
@@ -68,37 +135,49 @@ func parse (text string) []string {
     }
   }
 
-  result = append(result, str)
-  return result
+  tree.insertTree(str)
+  return tree
 }
 
 
 // ---
-func createBox (tokens []string) []string {
-  line, result := "", []string{"", "", ""}
-
-  for _, v := range tokens {
-    if containsToken(v) {
-      result[0] += "     "
-      result[1] += " " + v + " "
-      result[2] += "     "
-    } else {
-      for i := 0; i < len(v) + 6; i++ {
-        if i == 0 || i == len(v) + 5 {
-          line += "+"
-        } else {
-          line += "-"
-        }
-      }
-
-      result[0] += line
-      result[1] += "|  " + v + "  |"
-      result[2] += line
-      line = ""
-    }
+func createBox (t *Tree) [5]string {
+  var box [5]string
+  if t != nil {
+    constructBox(t.root, &box)
   }
-  return result
+  return box
 }
+
+func constructBox (n *Node, box *[5]string) {
+  if n.left != nil {
+    constructBox(n.left, box)
+  }
+
+  if n.token {
+    box[0] += "     "
+    box[1] += " " + n.text + " "
+    box[2] += "     "
+  } else {
+    line := ""
+    for i := 0; i < len(n.text) + 6; i++ {
+      if i == 0 || i == len(n.text) + 5 {
+        line += "+"
+      } else {
+        line += "-"
+      }
+    }
+
+    box[0] += line
+    box[1] += "|  " + n.text + "  |"
+    box[2] += line
+  }
+
+  if n.right != nil {
+    constructBox(n.right, box)
+  }
+}
+
 
 func containsToken (t string) bool {
   tokens := [...]string{
